a73e9f603b004fde1a129d0704dbc329360510e1
lauranutanix <laura@nutanix.com>
2025-12-10 09:09:30 -0800

    separate prod and local compose files

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ "$1" = "local" ]; then
-	docker compose  --profile local build
-	docker compose --profile local up -d
+	docker compose  build
+	docker compose up -d
 	echo "access at http://localhost:8000/"
 elif [ "$1" = "prod" ]; then
-	docker compose  --profile prod build
-	docker compose --profile prod up -d
+	docker compose -f docker-compose.prod.yaml build
+	docker compose -f docker-compose.prod.yaml up -d
 else
 	echo "Error: Please specify 'local' or 'prod' as the argument."
 	exit 1
--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 if [ "$1" = "local" ]; then
-	docker compose  --profile local down
+	docker compose  down
 elif [ "$1" = "prod" ]; then
-	docker compose  --profile prod down
+	docker compose -f docker-compose.prod.yaml down
 else
 	echo "Error: Please specify 'local' or 'prod' as the argument."
 	exit 1
